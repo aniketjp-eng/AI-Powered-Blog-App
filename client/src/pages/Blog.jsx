1851b73459562d71043d63fca884e5dbce711a42
Aniket Prajapati <aniketjp.eng@gmail.com>
2026-05-29 22:03:44 +0530

    Full Blog Page created

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -3,23 +3,33 @@ import { useParams } from "react-router-dom";
 import { assets, blog_data, comments_data } from "../assets/assets";
 import { Navbar } from "../components/Navbar";
 import moment from "moment";
+import Footer from '../components/Footer'
+import Loader from "../components/Loader";
 
 export const Blog = () => {
   const { id } = useParams();
   const [data, setData] = useState(null);
   const [comments, setComments] = useState([]);
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
 
   const fetchBlogData = async () => {
+    console.log("URL ID:", id);
     const blog = blog_data.find((item) => item._id == id);
+    console.log("Found Blog:", blog);
     setData(blog);
   };
-  const fetchComments = async () =>  {
+  const fetchComments = async () => {
     setComments(comments_data);
-  }
+  };
+
+  const addComment = async (e) => {
+    e.preventDefault();
+  };
   useEffect(() => {
-    fetchBlogData()
-    fetchComments()
-  }, []);
+    fetchBlogData();
+    fetchComments();
+  }, [id]);
   return data ? (
     <div className="relative">
       <img
@@ -28,9 +38,7 @@ export const Blog = () => {
         className="absolute -top-50 -z-1 opacity-50"
       />
       <Navbar />
-      {/* <div>
-          <p>Published on {moment(data.createdAt).format('MMMM Do YYYY')}</p>
-          </div> */}
+
       <div className="text-center mt-20 text-gray-600">
         <p className="text-primary py-4 font-medium">
           Published on {moment(data.createdAt).format("MMMM Do YYYY")}
@@ -47,38 +55,86 @@ export const Blog = () => {
         </p>
       </div>
 
-{/* ------------------------ */}
-      <div className='mx-5 max-w-5xl md:mx-auto my-10 mt-6'>
-          <img
-          src={data.image}
-          alt=""
-          className='rounded-3xl mb-5'
-         />
+      {/* ------------------------ */}
+      <div className="mx-5 max-w-5xl md:mx-auto my-10 mt-6">
+        <img src={data.image} alt="" className="rounded-3xl mb-5" />
+        <div
+          className="rich-text max-w-3xl mx-auto"
+          dangerouslySetInnerHTML={{ __html: data.description }}
+        />
 
-  <div className='rich-text max-w-3xl mx-auto' dangerouslySetInnerHTML=
-  {{ __html: data.description }}> </div>
-
-  {/* Comments Section */}
-  <div className='mt-14 mb-10 max-w-3xl mx-auto'>
-    <p>Comments({comments.length})</p>
-    <div className="flex flex-col gap-4">
-          {comments.map((item, index) => (
-            <div key={index} className='relative bg-primary/2 border border-primary/5 max-w-xl p-4
+        {/* Comments Section */}
+        <div className="mt-14 mb-10 max-w-3xl mx-auto">
+          <p className="font-semibold mb-4">Comments({comments.length})</p>
+          <div className="flex flex-col gap-4">
+            {comments.map((item, index) => (
+              <div
+                key={index}
+                className="relative bg-primary/2 border border-primary/5 max-w-xl p-4
             rounded text-gray-600
-            '>
-              <div>
-                <img src={assets.user_icon} className="w-6" alt="" />
-                <p className="font-medium">{item.name}</p>
+            "
+              >
+                <div className="flex items-center gap-2 mb-2">
+                  <img src={assets.user_icon} className="w-6" alt="" />
+                  <p className="font-medium">{item.name}</p>
+                </div>
+                <p className="text-sm max-w-md ml-8">{item.content}</p>
+                <div
+                  className="absolute right-4 bottom-3 flex item-center
+              gap-2 text-xs"
+                >
+                  {moment(item.createdAt).fromNow()}
+                </div>
               </div>
-               </div>
-          ) )}
-    </div>
-  </div>
-</div>
-
-{/* ------------------------- */}
+            ))}
+          </div>
+          {/* Add Comment Sections */}
+          <div className="max-w-3xl mx-auto">
+            <p className="font-semibold mb-4">Add you comment</p>
+            <form
+              onSubmit={addComment}
+              className="flex flex-col items-start gap-4 max-w-lg"
+            >
+            <input
+            onChange={(e) => setName(e.target.value)} value={name}
+              placeholder="Name"
+              required
+              class="w-full 
+p-2 border border-gray-300 rounded 
+outline-none"
+              type="text"
+            ></input>
+            <textarea
+             onChange={(e) => setContent(e.target.value)} value={content}
+              placeholder="Comment"
+              class="w-full p-2 border border-gray-300
+ rounded outline-none h-48"
+              required
+            ></textarea>
+            <button
+              type="submit"
+              class="bg-primary text-white rounded p-2 px-8 hover:scale-102 
+ transition-all cursor-pointer"
+            >
+              Submit
+            </button>
+            </form>
+          </div>
+          {/* Share Buttons  */}
+         <div class="my-24 max-w-3xl mx-auto">
+          <p class="font-semibold my-4">Share this article on social media</p>
+          <div class="flex">
+            <img width="50" alt src={assets.facebook_icon}></img>
+            <img width="50" alt src={assets.twitter_icon}></img>
+            <img width="50" alt src={assets.googleplus_icon}></img>
+            </div>
+          </div> 
+        </div>
+      </div>
+<Footer/>
+      {/* ------------------------- */}
     </div>
   ) : (
-    <div>Loading...</div>
+    <Loader/>
   );
 };
