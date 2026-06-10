ca9c0c07a700fcb78428cec2a5cd73f035c67e9b
Aniket Prajapati <aniketjp.eng@gmail.com>
2026-06-10 20:46:19 +0530

    some refinement

--- client/src/pages/admin/ListBlog.jsx
+++ client/src/pages/admin/ListBlog.jsx
@@ -2,6 +2,7 @@ import React, { useState,useEffect } from 'react';
 import { blog_data } from '../../assets/assets';
 import { useAppContext } from '../../context/AppContext';
 import BlogTableItem from '../../components/admin/BlogTableItem';
+import toast from 'react-hot-toast';
 
 const ListBlog = () => {
    const [blogs, setBlogs] = useState([]);
@@ -55,39 +56,6 @@ const fetchBlogs = async () =>{
                     </tbody>
           </table>
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-        {/* ======================================= */}
-        {/* <h1>All blogs</h1>
-
-        <div className='relative h-4/5 mt-4 max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide bg-white'>
-                <table className='w-full text-sm text-gray-500'>
-                    <thead className='text-xs text-gray-600 text-left uppercase'>
-                        <tr>
-                            <th scope='col' className='px-2 py-4 xl:px-6'> # </th>
-                            <th scope='col' className='px-2 py-4'> Blog Title </th>
-                            <th scope='col' className='px-2 py-4 max-sm:hidden'> Date </th>
-                            <th scope='col' className='px-2 py-4 max-sm:hidden'> Status </th>
-                            <th scope='col' className='px-2 py-4'> Actions </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {blogs.map((blog, index)=>{
-                            return <BlogTableItem key={blog._id} blog={blog} fetchBlogs={fetchBlogs} index={index + 1}/>
-                        })}
-                    </tbody>
-                </table>
-            </div> */}
     </div>
   )
 }
