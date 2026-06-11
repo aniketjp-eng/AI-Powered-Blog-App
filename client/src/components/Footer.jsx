d305853fed2930b408be0bd331f14d03d569f6a0
Aniket Prajapati <aniketjp.eng@gmail.com>
2026-06-11 17:18:03 +0530

    deployment error checking

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -26,7 +26,7 @@ function Footer() {
                 ))}
             </div>
         </div>
-        <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2026 © BlogForge - All Right Reserved.</p>
+        <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2026 ©  BlogForge - All Right Reserved.</p>
     </div>
   )
 }
